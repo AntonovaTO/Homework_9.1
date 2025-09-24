075b3b55953e2697498ff860f8b843dc25122afd
Tatiana Antonova <toffy2antonovato@gmail.com>
2025-09-24 11:11:16 +0300

    linters correction files in src

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,10 @@
-def filter_by_state(transactions: list[dict], state: str = 'EXECUTED') -> list[dict]:
+import os
+
+base_path = os.path.dirname(os.path.abspath(__file__))
+full_path = os.path.join(base_path, "src", "processing")
+
+
+def filter_by_state(transactions: list[dict], state: str = "EXECUTED") -> list[dict]:
     """
     Фильтрует список словарей по значению ключа 'state'.
 
@@ -9,7 +15,7 @@ def filter_by_state(transactions: list[dict], state: str = 'EXECUTED') -> list[d
     Returns:
         Список словарей, отфильтрованных по статусу
     """
-    return [transaction for transaction in transactions if transaction.get('state') == state]
+    return [transaction for transaction in transactions if transaction.get("state") == state]
 
 
 def sort_by_date(transactions: list[dict], reverse: bool = True) -> list[dict]:
@@ -23,14 +29,15 @@ def sort_by_date(transactions: list[dict], reverse: bool = True) -> list[dict]:
     Returns:
         Отсортированный список словарей
     """
-    return sorted(transactions, key=lambda x: x['date'], reverse=reverse)
+    return sorted(transactions, key=lambda x: x["date"], reverse=reverse)
+
 
 # Пример входных данных
 data = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
 
 # Фильтрация со статусом по умолчанию 'EXECUTED'
@@ -39,7 +46,7 @@ print(filter_by_state(data))
 #         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
 
 # Фильтрация со статусом 'CANCELED'
-print(filter_by_state(data, 'CANCELED'))
+print(filter_by_state(data, "CANCELED"))
 # Вывод: [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
 #         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
